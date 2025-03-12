8a4f1f90623c88ddbf6402542c125a1f55d387c4
Vikrant Lodhi <161944512+vikrantlodhi@users.noreply.github.com>
2025-03-12 12:40:41 +0530

    Update mywork_data.js

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,41 +1,47 @@
-import project1_img from '../assets/project_1.png'
-import project2_img from '../assets/project_2.png'
-import project3_img from '../assets/project_3.png'
-import project4_img from '../assets/project_4.png'
-import project5_img from '../assets/project_5.png'
-import project6_img from '../assets/project_6.png'
+import project1_img from '../assets/project_1.png';
+import project2_img from '../assets/project_2.png';
+import project3_img from '../assets/project_3.png';
+import project4_img from '../assets/project_4.png';
+import project5_img from '../assets/project_5.png';
+import project6_img from '../assets/project_6.png';
 
 const mywork_data = [
     {
-        w_no:1,
-        w_name:"Web design",
-        w_img:project1_img
+        w_no: 1,
+        w_name: "Web design",
+        w_img: project1_img,
+        w_link: "https://vikrant-lodhi.netlify.app/project-1/"
     },
     {
-        w_no:2,
-        w_name:"Web design",
-        w_img:project2_img
+        w_no: 2,
+        w_name: "Web development",
+        w_img: project2_img,
+        w_link: "https://vikrant-lodhi.netlify.app/project-2/"
     },
     {
-        w_no:3,
-        w_name:"Web design",
-        w_img:project3_img
+        w_no: 3,
+        w_name: "Mobile App",
+        w_img: project3_img,
+        w_link: "https://vikrant-lodhi.netlify.app/projects/task-management"
     },
     {
-        w_no:4,
-        w_name:"Web design",
-        w_img:project4_img
+        w_no: 4,
+        w_name: "E-commerce Website",
+        w_img: project4_img,
+        w_link: "https://vikrant-lodhi.netlify.app/projects/random-quote"
     },
     {
-        w_no:5,
-        w_name:"Web design",
-        w_img:project5_img
+        w_no: 5,
+        w_name: "Portfolio Website",
+        w_img: project5_img,
+        w_link: "https://vikrant-lodhi.netlify.app/projects/text-to-voice"
     },
     {
-        w_no:6,
-        w_name:"Web design",
-        w_img:project6_img
+        w_no: 6,
+        w_name: "Dashboard UI",
+        w_img: project6_img,
+        w_link: "https://vikrant-lodhi.netlify.app/projects/digital-clock"
     },
-]
- 
+];
+
 export default mywork_data;
