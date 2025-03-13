d16b8641f1194537359352f6c58ccfc5d45e1e7d
Vikrant Lodhi <161944512+vikrantlodhi@users.noreply.github.com>
2025-03-13 08:50:39 +0530

    Update services_data.js

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -2,32 +2,32 @@ const Services_Data = [
     {
         s_no:"01",
         s_name:"Web design",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Creating visually appealing and user-friendly interfaces..."
     },
     {
         s_no:"02",
         s_name:"Backend",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Developing server-side logic, APIs, and database intera..."
     },
     {
         s_no:"03",
         s_name:"Database Design",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Structuring data models, relationships, and queries to..."
     },
     {
         s_no:"04",
         s_name:"App design",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Crafting intuitive user experiences with well-structu..."
     },
     {
         s_no:"05",
         s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Engaging audiences through strategic content, trends..."
     },
     {
         s_no:"06",
         s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Crafting compelling and valuable content that informs..."
     },
 ]
  
